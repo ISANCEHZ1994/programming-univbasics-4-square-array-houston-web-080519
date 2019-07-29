04282fcec47870a889597efa686d4b805c358c77
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-29 21:29:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,16 +3,10 @@
 def square_array(array)
   x = 0
   
-  
-  while x <= array.length do
+  while x < array.length do
    array[x] = array[x]**array[x]
    
   x += 1 
-  
-  
-  
-  
-  
 end
    
 array
